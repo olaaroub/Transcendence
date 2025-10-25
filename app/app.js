323ff721c0e01dc7b340d7b6222ad8b8a0b204aa
OussamaHammou236 <oussamahammou112233@gmail.com>
2025-10-25 23:22:39 +0100

    fix the error of  cros policy ( I allowed all ips to request my backend )

--- app/app.js
+++ app/app.js
@@ -22,6 +22,7 @@ const start = async () => {
 
     fastify.decorate('db', db);
 
+<<<<<<< HEAD
     fastify.addHook('onRequest', async (request, reply) => {
       if (request.routerPath === '/login' || request.routerPath === '/signUp')
           return ;
@@ -33,6 +34,14 @@ const start = async () => {
         console.log("No token provided");
         reply.code(401).send({ error: 'No token provided' });
       }
+=======
+    fastify.addHook('onRequest', (request, reply, done) => {
+      // const token = "aiman";
+      // request.user = token;
+      if (request.routerPath === '/login' || request.routerPath === '/signUp')
+          return ;
+      done();
+>>>>>>> 0d78964 (fix the error of  cros policy ( I allowed all ips to request my backend ))
     });
 
     await routes(fastify);
