7817fd744a0789cc31d6db8097219c45401cdbfd
OussamaHammou236 <oussamahammou112233@gmail.com>
2025-11-02 17:03:44 +0100

    structer the code

--- app/routes/images.js
+++ app/routes/images.js
@@ -7,69 +7,82 @@ const { URL } = require('url');
 
 
 
-async function getProfileImages(fastify)
+async function getProfileImages(req, reply)
 {
-    fastify.register(fastify_static , {
-        root: path.join(__dirname, '../../static'),
-        prefix: '/public/',
-    });
-    fastify.get(`/users/:id/image`, async (req, reply) => {
-      const id = req.params.id;
-      const img = await fastify.db.get("SELECT profileImage FROM infos WHERE user_id = ?", id);
-      reply.code(200).send(img.profileImage);
-    });
+  try
+  {
+    const id = req.params.id;
+    const img = await fastify.db.get("SELECT profileImage FROM infos WHERE user_id = ?", id);
+    reply.code(200).send(img.profileImage);
   }
+  catch (err)
+  {
+    console.log(err);
+    reply.code(500).send({success: false, message: "internal server error"});
+  }
+}
 
 
 
-async function modifyAvatar(fastify)
+async function modifyAvatar(req, reply)
 {
-    await fastify.register(fastifyMultipart);
-    fastify.put(`/users/:id/image`, async (req, reply) => {
-        const id = req.params.id;
-        try {
-            const data = await fastify.db.get("SELECT profileImage FROM infos WHERE user_id = ?", id);
-            const imgpath = path.basename(new URL(data.profileImage).pathname);
-            console.log(path.join(__dirname, '../../static', imgpath));
-            if (imgpath != `Default_pfp.jpg`)
-              await fs.promises.unlink(path.join(__dirname, '../../static', imgpath));
-        } catch (err)
-        {
+  const id = req.params.id;
+  try
+  {
+    const data = await this.db.get("SELECT profileImage FROM infos WHERE user_id = ?", id);
+    const imgpath = path.basename(new URL(data.profileImage).pathname);
+    console.log(path.join(__dirname, '../../static', imgpath));
+    if (imgpath != `Default_pfp.jpg`)
+      await fs.promises.unlink(path.join(__dirname, '../../static', imgpath));
+    const datafile = await req.file();
+    const ext = path.extname(datafile.filename);
+    const file_name = uuidv4() + ext;
+
+    const file_path = path.join(__dirname, '../../static', file_name);
+    await fs.promises.writeFile(file_path, await datafile.toBuffer());
+
+    const imageUri = `http://127.0.0.1:3000/public/${file_name}`;
+
+    await this.db.run("UPDATE infos SET profileImage = ?  WHERE user_id = ?", [imageUri, id]);
+    reply.code(201).send({success: true, message: "your update the profile image successfully"});
+  }
+  catch (err)
+  {
           reply.code(500).send({success: false, message: err});
           console.log(err);
-        }
-        const data = await req.file();
-        const ext = path.extname(data.filename);
-        const file_name = uuidv4() + ext;
-
-        const file_path = path.join(__dirname, '../../static', file_name);
-        await fs.promises.writeFile(file_path, await data.toBuffer());
-
-        const imageUri = `http://127.0.0.1:3000/public/${file_name}`;
-
-        await fastify.db.run("UPDATE infos SET profileImage = ?  WHERE user_id = ?", [imageUri, id]);
-        reply.code(201).send({success: true, message: "your update the profile image successfully"});
-    });
+  }
 }
 
-async function deleteAvatar(fastify)
+async function deleteAvatar(req, reply)
 {
-  fastify.delete('/users/:id/image', async (req, reply) => {
-    const id = req.params.id;
-    try {
-      const data = await fastify.db.get("SELECT profileImage FROM infos WHERE user_id = ?", id);
-      const imgpath = path.basename(new URL(data.profileImage).pathname);
-      console.log(path.join(__dirname, '../../static', imgpath));
-      if (imgpath == `Default_pfp.jpg`)
-          reply.code(401).send({success: false, message: "can't delete the default img"});
-      await fs.promises.unlink(path.join(__dirname, '../../static', imgpath));
-      await fastify.db.run("UPDATE infos SET profileImage = ? WHERE user_id = ?", ["http://127.0.0.1:3000/public/Default_pfp.jpg", id]);
-      reply.code(201).send({success: true, message: "your delete the profile image successfully"});
-    } catch (err) {
-      reply.code(500).send({success: false, message: err});
-      console.log(err);
-    }
-  })
+  const id = req.params.id;
+  try
+  {
+    const data = await this.db.get("SELECT profileImage FROM infos WHERE user_id = ?", id);
+    const imgpath = path.basename(new URL(data.profileImage).pathname);
+    console.log(path.join(__dirname, '../../static', imgpath));
+    if (imgpath == `Default_pfp.jpg`)
+      reply.code(401).send({success: false, message: "can't delete the default img"});
+    await fs.promises.unlink(path.join(__dirname, '../../static', imgpath));
+    await this.db.run("UPDATE infos SET profileImage = ? WHERE user_id = ?", ["http://127.0.0.1:3000/public/Default_pfp.jpg", id]);
+    reply.code(201).send({success: true, message: "your delete the profile image successfully"});
+  }
+  catch (err) {
+    reply.code(500).send({success: false, message: err});
+    console.log(err);
+  }
 }
 
-module.exports =  {getProfileImages, modifyAvatar, deleteAvatar};
+async function routes (fastify)
+{
+  await fastify.register(fastify_static , {
+    root: path.join(__dirname, '../../static'),
+    prefix: '/public/',
+  });
+  await fastify.register(fastifyMultipart);
+  fastify.delete('/users/:id/image', deleteAvatar);
+  fastify.put(`/users/:id/image`, modifyAvatar);
+  fastify.get(`/users/:id/image`, getProfileImages);
+}
+
+module.exports =  routes;
--- app/routes/mainRoutes.js
+++ app/routes/mainRoutes.js
@@ -1,17 +1,10 @@
-const users = require('./users');
-const images = require("./images");
-const search_bar = require("./search");
 
 async function Routes  (fastify)
 {
-    await users.sign_up(fastify);
-    await users.login(fastify);
-    await users.getUsers(fastify);
-    await images.getProfileImages(fastify);
-    await images.modifyAvatar(fastify);
-    await images.deleteAvatar(fastify);
-    await search_bar(fastify);
     fastify.register(require('./settings'));
+    fastify.register(require('./users'));
+    fastify.register(require('./images'));
+    fastify.register(require('./search'))
 }
 
 module.exports = Routes;
