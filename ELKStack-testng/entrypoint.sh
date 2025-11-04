be16a59077b0b242248b6243e20af5c27128fd94
Oussama Laaroubi <oussamalaarroubi@gmail.com>
2025-11-04 13:49:49 +0100

    testing elk with random logs, still need to implement other log levels and services

--- ELKStack-testng/entrypoint.sh
+++ ELKStack-testng/entrypoint.sh
@@ -14,7 +14,15 @@ done
 echo "Logstash is ready."
 
 echo "Starting log stream..."
+count=0
 while true; do
-  echo "{\"message\": \"This is a log message at $(date)\", \"service\": \"log-generator\"}"
+
+  if [ $(($count % 4)) -eq 0 ]; then
+    echo "{\"level\": \"error\", \"service\": \"log-generator\", \"message\": \"Fuuck, something broke!\"}"
+  else
+    echo "{\"level\": \"info\", \"service\": \"log-generator\", \"message\": \"This is a log message at $(date)\"}"
+  fi
+
+  count=$(($count + 1))
   sleep 5
 done | nc logstash 5000
